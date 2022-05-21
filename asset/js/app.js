1eb964ab69fa0f3f7ef84188da21de0a8edb916a
alicalimli <alicalimli76@gmail.com>
2022-05-21 11:50:13 +0300

    prettier code format

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,163 +1,157 @@
 // Initial Values
-const INITIAL_SEARCH_VALUE = 'spiderman';
+const INITIAL_SEARCH_VALUE = "spiderman";
 const log = console.log;
 
 // Selecting elements from the DOM
-const searchButton = document.querySelector('#search');;
-const searchInput = document.querySelector('#exampleInputEmail1');
-const moviesContainer = document.querySelector('#movies-container');
-const moviesSearchable = document.querySelector('#movies-searchable');
+const searchButton = document.querySelector("#search");
+const searchInput = document.querySelector("#exampleInputEmail1");
+const moviesContainer = document.querySelector("#movies-container");
+const moviesSearchable = document.querySelector("#movies-searchable");
 
 function createImageContainer(imageUrl, id) {
-    const tempDiv = document.createElement('div');
-    tempDiv.setAttribute('class', 'imageContainer');
-    tempDiv.setAttribute('data-id', id);
+  const tempDiv = document.createElement("div");
+  tempDiv.setAttribute("class", "imageContainer");
+  tempDiv.setAttribute("data-id", id);
 
-    const movieElement = `
+  const movieElement = `
         <img src="${imageUrl}" alt="" data-movie-id="${id}">
     `;
-    tempDiv.innerHTML = movieElement;
+  tempDiv.innerHTML = movieElement;
 
-    return tempDiv;
+  return tempDiv;
 }
 
 function resetInput() {
-    searchInput.value = '';
+  searchInput.value = "";
 }
 
 function handleGeneralError(error) {
-    log('Error:', error.message);
-    log(error.message || 'Internal Server');
+  log("Error:", error.message);
+  log(error.message || "Internal Server");
 }
 
 function createIframe(video) {
-    const videoKey = (video && video.key) || 'No key found!!!';
-    const iframe = document.createElement('iframe');
-    iframe.src = `https://www.youtube.com/embed/${videoKey}`;
-    iframe.width = 360;
-    iframe.height = 315;
-    iframe.allowFullscreen = true;
-    return iframe;
+  const videoKey = (video && video.key) || "No key found!!!";
+  const iframe = document.createElement("iframe");
+  iframe.src = `https://www.youtube.com/embed/${videoKey}`;
+  iframe.width = 360;
+  iframe.height = 315;
+  iframe.allowFullscreen = true;
+  return iframe;
 }
 
 function insertIframeIntoContent(video, content) {
-    const videoContent = document.createElement('div');
-    const iframe = createIframe(video);
+  const videoContent = document.createElement("div");
+  const iframe = createIframe(video);
 
-    videoContent.appendChild(iframe);
-    content.appendChild(videoContent);
+  videoContent.appendChild(iframe);
+  content.appendChild(videoContent);
 }
 
-
 function createVideoTemplate(data) {
-    const content = this.content;
-    content.innerHTML = '<p id="content-close">X</p>';
+  const content = this.content;
+  content.innerHTML = '<p id="content-close">X</p>';
 
-    const videos = data.results || [];
+  const videos = data.results || [];
 
-    if (videos.length === 0) {
-        content.innerHTML = `
+  if (videos.length === 0) {
+    content.innerHTML = `
             <p id="content-close">X</p>
             <p>No Trailer found for this video id of ${data.id}</p>
         `;
-        return;
-    }
+    return;
+  }
 
-    for (let i = 0; i < 4; i++) {
-        const video = videos[i];
-        insertIframeIntoContent(video, content);
-    }
+  for (let i = 0; i < 4; i++) {
+    const video = videos[i];
+    insertIframeIntoContent(video, content);
+  }
 }
 
 function createSectionHeader(title) {
-    const header = document.createElement('h2');
-    header.innerHTML = title;
+  const header = document.createElement("h2");
+  header.innerHTML = title;
 
-    return header;
+  return header;
 }
-
 
 function renderMovies(data) {
-    const moviesBlock = generateMoviesBlock(data);
-    const header = createSectionHeader(this.title);
-    moviesBlock.insertBefore(header, moviesBlock.firstChild);
-    moviesContainer.appendChild(moviesBlock);
+  const moviesBlock = generateMoviesBlock(data);
+  const header = createSectionHeader(this.title);
+  moviesBlock.insertBefore(header, moviesBlock.firstChild);
+  moviesContainer.appendChild(moviesBlock);
 }
 
-
-
 function renderSearchMovies(data) {
-    moviesSearchable.innerHTML = '';
-    const moviesBlock = generateMoviesBlock(data);
-    moviesSearchable.appendChild(moviesBlock);
+  moviesSearchable.innerHTML = "";
+  const moviesBlock = generateMoviesBlock(data);
+  moviesSearchable.appendChild(moviesBlock);
 }
 
 function generateMoviesBlock(data) {
-    const movies = data.results;
-    const section = document.createElement('section');
-    section.setAttribute('class', 'section');
+  const movies = data.results;
+  const section = document.createElement("section");
+  section.setAttribute("class", "section");
 
-    for (let i = 0; i < movies.length; i++) {
-        const { poster_path, id } = movies[i];
+  for (let i = 0; i < movies.length; i++) {
+    const { poster_path, id } = movies[i];
 
-        if (poster_path) {
-            const imageUrl = MOVIE_DB_IMAGE_ENDPOINT + poster_path;
+    if (poster_path) {
+      const imageUrl = MOVIE_DB_IMAGE_ENDPOINT + poster_path;
 
-            const imageContainer = createImageContainer(imageUrl, id);
-            section.appendChild(imageContainer);
-        }
+      const imageContainer = createImageContainer(imageUrl, id);
+      section.appendChild(imageContainer);
     }
+  }
 
-    const movieSectionAndContent = createMovieContainer(section);
-    return movieSectionAndContent;
+  const movieSectionAndContent = createMovieContainer(section);
+  return movieSectionAndContent;
 }
-
-
 
 // Inserting section before content element
 function createMovieContainer(section) {
-    const movieElement = document.createElement('div');
-    movieElement.setAttribute('class', 'movie');
+  const movieElement = document.createElement("div");
+  movieElement.setAttribute("class", "movie");
 
-    const template = `
+  const template = `
         <div class="content">
             <p id="content-close">X</p>
         </div>
     `;
 
-    movieElement.innerHTML = template;
-    movieElement.insertBefore(section, movieElement.firstChild);
-    return movieElement;
+  movieElement.innerHTML = template;
+  movieElement.insertBefore(section, movieElement.firstChild);
+  return movieElement;
 }
 
-searchButton.onclick = function(event) {
-    event.preventDefault();
-    const value = searchInput.value
+searchButton.onclick = function (event) {
+  event.preventDefault();
+  const value = searchInput.value;
 
-    if (value) {
-        searchMovie(value);
-    }
-    resetInput();
-}
+  if (value) {
+    searchMovie(value);
+  }
+  resetInput();
+};
 
 // Click on any movies
 // Event Delegation
-document.onclick = function(event) {
-    log('Event: ', event);
-    const { tagName, id } = event.target;
-    if (tagName.toLowerCase() === 'img') {
-        const movieId = event.target.dataset.movieId;
-        const section = event.target.parentElement.parentElement;
-        const content = section.nextElementSibling;
-        content.classList.add('content-display');
-        getVideosByMovieId(movieId, content);
-    }
+document.onclick = function (event) {
+  log("Event: ", event);
+  const { tagName, id } = event.target;
+  if (tagName.toLowerCase() === "img") {
+    const movieId = event.target.dataset.movieId;
+    const section = event.target.parentElement.parentElement;
+    const content = section.nextElementSibling;
+    content.classList.add("content-display");
+    getVideosByMovieId(movieId, content);
+  }
 
-    if (id === 'content-close') {
-        const content = event.target.parentElement;
-        content.classList.remove('content-display');
-    }
-}
+  if (id === "content-close") {
+    const content = event.target.parentElement;
+    content.classList.remove("content-display");
+  }
+};
 
 // Initialize the search
 //searchMovie(INITIAL_SEARCH_VALUE);
