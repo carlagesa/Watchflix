5e4746372bbdecc0f899b4889720557a1e680d4b
Carlton Agesa <65759360+carlagesa@users.noreply.github.com>
2024-05-18 11:54:59 +0300

    API key

--- asset/js/tvapiTransaction.js
+++ asset/js/tvapiTransaction.js
@@ -1,5 +1,5 @@
 // Initial Values
-const MOVIE_DB_API = "d8bf019d0cca372bd804735f172f67e8";
+const MOVIE_DB_API = "354399156ae839327cb9ce5ca28b7196";
 const MOVIE_DB_ENDPOINT = "https://api.themoviedb.org";
 const MOVIE_DB_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w500";
 const DEFAULT_POST_IMAGE = "https://via.placeholder.com/150";
